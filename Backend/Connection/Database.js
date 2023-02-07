89843b358b2e3d4f090e1ff6f6dfa85e59be7ebc
Robert647 <robertoloxa88@gmal.com>
2023-02-06 19:11:59 -0600

    changes

--- Backend/Connection/Database.js
+++ Backend/Connection/Database.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose')
 const { ConnectionCloud } = require('../Config')
- 
-const options = {
-  autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-}
 
 function GetConnection() {
-  mongoose.set('strictQuery',true)
-  mongoose.connect(ConnectionCloud)
-  mongoose.connection.on('open', ()=>{
+  mongoose.set('strictQuery', true)
+  mongoose.connect(ConnectionCloud, { useNewUrlParser: true })
+  mongoose.connection.on('open', () => {
     console.log("Connection to Database succes.")
   })
-  mongoose.connection.on('error', err =>{
+  mongoose.connection.on('error', err => {
     console.error(err)
   })
 }
--- Backend/Models/Model.js
+++ Backend/Models/Model.js
@@ -1,6 +1,54 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Usuario = new Schema({
-  
+const user = new Schema({
+  key: { type: String, require: true, unique: true },
+  username: { type: String, require: true, unique: true },
+  name: { type: String, require: true },
+  surnames: { type: String, require: true },
+  date: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  phone: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  type: { type: String, default: "cliente", require: true },
 })
+
+const product = new Schema({
+  key: { type: String, require: true, unique: true },
+  name: { type: String, require: true, unique: true },
+  categorykey: { type: String, require: true },
+  stock: { type: Number, require: true },
+  company: { type: String, require: true },
+  details: { type: String, require: true },
+  price: { type: Number, require: true },
+})
+
+const category = new Schema({
+  key: { type: String, require: true, unique: true },
+  name: { type: String, require: true, unique: true }
+})
+
+const sale = new Schema({
+  key: { type: String, require: true, unique: true },
+  clientkey: { type: String, require: true, },
+  cantproducts: { type: Number, require: true },
+  subtotal: { type: Number, require: true },
+  total: { type: Number, require: true },
+  state: { type: String, require: true, default: "Proceso" },
+})
+
+const detailsale = new Schema({
+  salekey: { type: String, require: true },
+  productkey: { type: String, require: true },
+  cant: { type: Number, require: true },
+  subtotal: { type: Number, require: true },
+  total: { type: Number, require: true },
+})
+
+module.exports = {
+  users: mongoose.model("users", user),
+  products: mongoose.model("products", product),
+  categories: mongoose.model("categories", category),
+  sales: mongoose.model("sales", sale),
+  detailsales: mongoose.model("detailsales", detailsale)
+}
