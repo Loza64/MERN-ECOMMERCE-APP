1f29e92d4cd56c86cb08dcd5b4183cb15e3a4d89
Robert647 <robertoloxa88@gmal.com>
2023-02-07 16:08:26 -0600

    changes

--- Backend/App.js
+++ Backend/App.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser')
+const routes = require('./Routes/api.routes')
 const { PORT } = require('../Backend/Config')
 const fileupload = require('express-fileupload')
 const GetConnection = require('./Connection/Database')
 const ServerApp = express()
 
 GetConnection()
+ServerApp.use(routes)
 ServerApp.use(bodyparser.json())
 ServerApp.use(bodyparser.urlencoded({ extended: true }))
 ServerApp.use(fileupload({ useTempFiles: true, tempFileDir: './Images' }))
