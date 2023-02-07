a07159d3c656640de214a5a2593cff68e8552bf5
Robert647 <robertoloxa88@gmal.com>
2023-02-07 16:57:00 -0600

    changes

--- Backend/Routes/api.routes.js
+++ Backend/Routes/api.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { SignUp, Login } = require('../Controllers/controller')
 const routes = express.Router()
 
-routes.post('Login', Login)
-routes.post('SignUp', SignUp)
+routes.post('/Login', Login)
+routes.post('/SignUp', SignUp)
 
 module.exports = routes
