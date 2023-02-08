b327f3a1979c954149e21fcb68a376062706ecd7
Robert647 <robertoloxa88@gmal.com>
2023-02-08 10:47:06 -0600

    changes

--- Backend/App.js
+++ Backend/App.js
@@ -6,12 +6,12 @@ const fileupload = require('express-fileupload')
 const GetConnection = require('./Connection/Database')
 const ServerApp = express()
 
+GetConnection()
 ServerApp.use(bodyparser.json())
+ServerApp.use('/ServerCommerce', routes)
 ServerApp.use(bodyparser.urlencoded({ extended: true }))
 ServerApp.use(fileupload({ useTempFiles: true, tempFileDir: './Images' }))
 
-GetConnection()
-ServerApp.use('/ServerCommerce', routes)
 ServerApp.get('/', (req, res) => {
   res.send(`The Server is running on port: ${PORT}`);
 })
