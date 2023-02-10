dd978c86f5d4c74880eb540fc7f60af2a5ff20a9
Robert647 <robertoloxa88@gmal.com>
2023-02-09 22:18:23 -0600

    changes

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import ContextConsumer from './Context/Context';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
+    <ContextConsumer>
+      <Router>
+        <App />
+      </Router>
+    </ContextConsumer>
   </React.StrictMode>
 );
