3c0d98de2531ff48b6b44686f3e2c22a0d940047
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-04-16 08:12:25 -0600

    changes

--- Frontend/src/Context/ContextReducer.jsx
+++ Frontend/src/Context/ContextReducer.jsx
@@ -9,13 +9,11 @@ export function ContextReducer(state, action) {
   switch (action.type) {
     case Actions.ADD_TO_CART: {
       let newItem = action.payload;
-      let checkProduct = state.cart.find(
-        (itemproduct) => itemproduct.key === newItem.key
-      );
+      let checkProduct = state.cart.find((itemproduct) => itemproduct.key === newItem.key);
 
-      let MyCart = checkProduct
-        ? state.cart.map((item) =>
-          item.key === newItem.key ? { ...item, quantity: item.quantity + 1 } : item) : [...state.cart, newItem];
+      let MyCart = checkProduct ? state.cart.map((item) =>
+        (item.key === newItem.key ? { ...item, quantity: item.quantity + 1 } : item)) : 
+        [...state.cart, newItem];
 
       localStorage.setItem("cart", JSON.stringify(MyCart));
       return { ...state, cart: MyCart };
