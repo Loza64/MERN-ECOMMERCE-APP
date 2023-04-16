6b7761655bfde5b8c72e3ad0b348883259564f5c
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-04-16 09:11:48 -0600

    new function to the cart

--- Frontend/src/Components/Cart/Cart.jsx
+++ Frontend/src/Components/Cart/Cart.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Pago from "../Pago";
 import CartList from './CartList'
+import { ContextProvider } from "../../Context/Context";
 
 export default function Cart() {
-  return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <h1 className="text-center title-cart">Shopping Cart</h1>
-      <div className="flex-cart">
-        <CartList />
-        <Pago />
+  const { cart } = ContextProvider();
+  if (cart.length === 0) {
+    return (
+      <div className="cart-empty">
+        <br />
+        <br />
+        <br />
+        <label className="message">Your cart is currently empty.</label>
       </div>
-    </div>
-  );
+    )
+  } else {
+    return (
+      <div>
+        <br />
+        <br />
+        <br />
+        <h1 className="text-center title-cart">Shopping Cart</h1>
+        <div className="flex-cart">
+          <CartList />
+          <Pago />
+        </div>
+      </div>
+    )
+  }
 }
