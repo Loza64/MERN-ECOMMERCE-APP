0ff753faae9a21daf2cecd1d14e608c594891283
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-04-27 20:36:33 -0600

    change routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,10 +24,10 @@ export default function App() {
         <Route path='/Terms' element={<Terms />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Shoppings' element={<MyShoppings />} />
-        <Route path='/Products' element={<Products SubTopic={"Our"} Topic={"Products"} Top={'40px'} TopState={true} />} />
         <Route path='/Categories' element={<Categories SubTopic={"Look at our"} Topic={"Categories"} />} />
-        {categories.map(category => (<Route path={`/${category.name}`} element={<ProductsByCategorie key={category._id} CategoryKey={`${category.key}`} />} />))}
+        <Route path='/Products' element={<Products SubTopic={"Our"} Topic={"Products"} Top={'40px'} TopState={true} />} />
         {products.map(product => (<Route path={`/${product.name}`} element={<DetailProduct key={product._id} productkey={`${product.key}`} />} />))}
+        {categories.map(category => (<Route path={`/${category.name}`} element={<ProductsByCategorie key={category._id} CategoryKey={`${category.key}`} />} />))}
       </Routes>
     </div>
   );
