f5dd4dea3a2a4edef678684bc5c6f18adade027e
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-07-21 21:34:21 -0600

    changes

--- Frontend/src/Components/Loading.jsx
+++ Frontend/src/Components/Loading.jsx
@@ -4,7 +4,7 @@ export default function Loading({ title }) {
     return (
         <div className="loading-container">
             <div className="loading">
-                <img src="https://res.cloudinary.com/ufostart-development/image/upload/v1689982703/ECOMMERCE/mckaustcnmrms3nxvhzx.gif" alt="" />
+                <img src="https://res.cloudinary.com/ufostart-development/image/upload/v1689982703/ECOMMERCE/mckaustcnmrms3nxvhzx.gif" alt="Loading" />
                 <label>{title}</label>
             </div>
         </div>
