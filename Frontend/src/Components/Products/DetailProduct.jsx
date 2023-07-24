87a91b3487b07e5e990e9ca7cb45865185dbdf87
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-07-24 13:58:02 -0600

    Update load DetailProduct

--- Frontend/src/Components/Products/DetailProduct.jsx
+++ Frontend/src/Components/Products/DetailProduct.jsx
@@ -6,61 +6,91 @@ import { FaShoppingBag } from 'react-icons/fa'
 import { ContextProvider } from "../../Context/Context";
 import { DetailProducts } from "../Styles/styled-components";
 import ProductsByCategorie from "../Categories/ProductsByCategorie";
+import Loading from "../Loading";
+import ProductItem from "./ProductItem";
+import Swal from "sweetalert2";
 
 export default function DetailProduct({ product }) {
   const { key, image, name, company, categorykey, price, stock, discount, details } = product;
-  const { AddToCart } = ContextProvider();
+  const { AddToCart, getProductByCategorie, productsByCategorie } = ContextProvider();
   const navigator = useNavigate();
   const [view, setView] = useState(false);
+  const [time, setTime] = useState(0);
+  const timeId = setTimeout(() => { setTime(time + 1) }, 1000);
 
-  return (
-    <div>
-      <DetailProducts status={stock > 0 ? true : false} data={view}>
-        <Top state={true} />
-        <div className="content-detail-product">
-          <h2>{name}</h2>
-          <div className="flex-detail-product">
-            <div className="image">
-              <img src={image.url} alt={name} />
-            </div>
-            <div className="info">
-              <label className="company">Made by: <label className="detail">{company}</label></label>
-              <label className="price">Price: <label className="detail">${price}</label></label>
-              {discount > 0 ? (<label className="discount">Discount: <label className="detail">{Math.round(discount * 100)}%</label></label>) : null}
-              <label className="status">Status: <label className="detail">{stock > 0 ? "In Stock" : "Unavailable"}</label></label>
-              {
-                details.length >= 980 ?
-                  (
-                    <div className="description less">
-                      <p>{details}</p>
-                      <label className="view-more" onClick={() => { setView(view ? false : true) }}>
-                        {
-                          view ? ("View less") : ("View more")
-                        }
-                      </label>
-                    </div>
-                  ) : (
-                    <div className="description">
-                      <p>{details}</p>
-                    </div>
-                  )
-              }
-              {
-                stock > 0 ?
-                  (
-                    <div className="buttoms">
-                      <button className="buttom cart" onClick={() => { if (stock > 0) { AddToCart(key) } }} en>Add to cart <BsFillCartPlusFill className="react-icon" /></button>
-                      <button className="buttom buy" onClick={() => { if (stock > 0) { AddToCart(key); navigator('/cart') } }}>Buy now<FaShoppingBag className="react-icon" /></button>
-                    </div>
-                  ) : null
-              }
+  getProductByCategorie(categorykey).catch((err) => {
+    Swal.fire({
+      title: 'Connection server error',
+      text: 'Bug name: ' + err + ', we will solve this problem as soon as possible.',
+      icon: 'error',
+      button: "Aceptar",
+      footer: '<a href="mailto:ufostartservices@gmail.com">Report problem</a>'
+    })
+  });
+
+  if (time >= 1) {
+    clearTimeout(timeId);
+    return (
+      <div>
+        <DetailProducts status={stock > 0 ? true : false} data={view}>
+          <Top state={true} />
+          <div className="content-detail-product">
+            <h2>{name}</h2>
+            <div className="flex-detail-product">
+              <div className="image">
+                <img src={image.url} alt={name} />
+              </div>
+              <div className="info">
+                <label className="company">Made by: <label className="detail">{company}</label></label>
+                <label className="price">Price: <label className="detail">${price}</label></label>
+                {discount > 0 ? (<label className="discount">Discount: <label className="detail">{Math.round(discount * 100)}%</label></label>) : null}
+                <label className="status">Status: <label className="detail">{stock > 0 ? "In Stock" : "Unavailable"}</label></label>
+                {
+                  details.length >= 980 ?
+                    (
+                      <div className="description less">
+                        <p>{details}</p>
+                        <label className="view-more" onClick={() => { setView(view ? false : true) }}>
+                          {
+                            view ? ("View less") : ("View more")
+                          }
+                        </label>
+                      </div>
+                    ) : (
+                      <div className="description">
+                        <p>{details}</p>
+                      </div>
+                    )
+                }
+                {
+                  stock > 0 ?
+                    (
+                      <div className="buttoms">
+                        <button className="buttom cart" onClick={() => { if (stock > 0) { AddToCart(key) } }} en>Add to cart <BsFillCartPlusFill className="react-icon" /></button>
+                        <button className="buttom buy" onClick={() => { if (stock > 0) { AddToCart(key); navigator('/cart') } }}>Buy now<FaShoppingBag className="react-icon" /></button>
+                      </div>
+                    ) : null
+                }
+              </div>
             </div>
           </div>
-        </div>
-      </DetailProducts>
-      <h1 className="text-center" style={{ fontWeight: "bold" }}><label style={{ color: "blue", fontWeight: "bold" }}>Releated</label> Products</h1>
-      <ProductsByCategorie CategoryKey={categorykey} top={false} style={{ marginTop: "-100px" }} />
-    </div>
-
-  )
+        </DetailProducts>
+        {
+          productsByCategorie.length > 0 ?
+            (
+              <div>
+                <h1 className="text-center" style={{ fontWeight: "bold" }}><label style={{ color: "blue", fontWeight: "bold" }}>Releated</label> Products</h1>
+                <div className="grid">
+                  {
+                    productsByCategorie.map(item => (<ProductItem product={item} animationState={true}/>))
+                  }
+                </div>
+              </div>
+            ) : null
+        }
+      </div>
+    )
+  } else {
+    return <Loading title={"Loading..."} />
+  }
 }
