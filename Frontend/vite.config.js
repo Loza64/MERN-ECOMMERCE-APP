e58bfb5e2bbe7e611f6e1fa36a6274714ca7a330
Loza626 <00002522@uca.edu.sv>
2023-09-02 11:30:45 -0600

    fix proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    '/': {
-      target: 'http://localhost:4000/',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\//, ''),
-    },
+    proxy: {
+      '/': {
+        target: 'http://localhost:4000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\//, ''),
+      }
+    }
   }
 })
