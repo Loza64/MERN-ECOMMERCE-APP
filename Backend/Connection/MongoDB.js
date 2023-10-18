809b20a7c163bb9937d6dce8c81b8b30580e78ea
Loza64 <00002522@uca.edu.sv>
2023-10-18 12:14:38 -0600

    changes

--- Backend/Connection/MongoDB.js
+++ Backend/Connection/MongoDB.js
@@ -4,10 +4,6 @@ import { ConnectionCloud } from '../Config.js'
 export default function GetMongoConnection() {
   mongoose.set('strictQuery', true)
   mongoose.connect(ConnectionCloud)
-  mongoose.connection.on('open', () => {
-    console.log("Connection to Database succes.")
-  })
-  mongoose.connection.on('error', err => {
-    console.error(err)
-  })
+  mongoose.connection.on('open', () => { console.log("Connection database succes.") })
+  mongoose.connection.on('error', err => { console.error(`Connection database error: ${err}`) })
 }
