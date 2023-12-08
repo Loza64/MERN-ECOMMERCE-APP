fb3d98f182b8b4ba0593cd90c3ddf83a743fae38
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-12-08 16:29:04 -0600

    changes

--- Backend/src/Application.js
+++ Backend/src/Application.js
@@ -1,7 +1,5 @@
-//import path from 'path'
 import cors from 'cors'
 import express from 'express'
-//import { fileURLToPath } from 'url'
 import bodyparser from 'body-parser'
 import routes from './Routes/api.routes.js'
 import fileupload from 'express-fileupload'
@@ -10,8 +8,6 @@ import { Origin } from './SettingsEnv.js'
 import morgan from 'morgan'
 
 const ServerApp = express()
-//const __filename = fileURLToPath(import.meta.url)
-//const __dirname = path.dirname(__filename)
 
 //Configuration app
 GetMongoConnection()
@@ -21,9 +17,5 @@ ServerApp.use(bodyparser.json({ limit: '100mb', extended: true }))
 ServerApp.use(bodyparser.urlencoded({ limit: '100mb', extended: true }))
 ServerApp.use(fileupload({ useTempFiles: true, tempFileDir: './Resources' }))
 ServerApp.use('/backend/api/rest/ufostartserver/node/route/fetch/axios', routes)
-
-//Get files statics from Frontend
-//ServerApp.use(express.static(path.join(__dirname, '../Frontend/dist')))
-//ServerApp.get('*', (req, res) => res.sendFile(path.join(__dirname, '../Frontend/dist/index.html')))
 
 export default ServerApp;
