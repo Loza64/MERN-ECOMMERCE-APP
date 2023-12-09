ad46d9538a98ecba51fbdbada55865476273243c
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-12-08 19:30:51 -0600

    validation complete

--- Backend/src/Middlewares/VaidationsResult.js
+++ Backend/src/Middlewares/VaidationsResult.js
@@ -3,10 +3,11 @@ import { validationResult } from 'express-validator'
 
 const error = debug('backend:[Error]')
 
-export const ValidationResult = (req, res) => {
+export const ValidationResult = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         errors.array().map(item => error(item.msg))
         res.status(400).json({ state: false, message: errors.array().map(item => item.msg) });
     }
+    next();
 }
