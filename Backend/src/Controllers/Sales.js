46db966cdaeb076a193845a1bb133b2a7f2b4c38
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-12-17 13:24:20 -0600

    update

--- Backend/src/Controllers/Sales.js
+++ Backend/src/Controllers/Sales.js
@@ -7,18 +7,17 @@ export const GenerateSale = async (req, res, next) => {
     const details = req.session.cart
     const key = uniquid()
     try {
-        const sales = await new Sales({ key, user, date, products, subtotal, total, details }).save()
-        if (sales) {
+        new Sales({ key, user, date, products, subtotal, total, details }).save().then(sales => {
             sales.details.map(async (item) => {
                 const product = await Products.findById(item.id)
                 if (product) {
                     const newStock = product.stock - item.quantity > 0 ? product.stock - item.quantity : 0
                     Products.findByIdAndUpdate(item.id, { stock: newStock }).then((data) => {
-                        console.log(data.name + " stock updated")
+                        console.log(`The stock product went from ${data.stock} to ${newStock}`)
                     })
                 }
             });
-        }
+        })
     } catch (error) {
         next(error.message)
         return res.status(500).json({ state: false, message: error.message })
