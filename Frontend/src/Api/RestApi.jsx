541a1343e740c9ea9bfe094965991ac89951a124
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-12-23 18:03:48 -0600

    update axios

--- Frontend/src/Api/RestApi.jsx
+++ Frontend/src/Api/RestApi.jsx
@@ -5,7 +5,7 @@ const ServerFetch = axios.create({
     baseURL: ServerRoute,
     withCredentials: true,
     timeout: 1000 * 60 * 5,
-    headers: { "Content-Type": "multipart/form-data" },
+    headers: { "Content-Type": "application/json;charset=UTF-8" },
     timeoutErrorMessage: "Server timeout has expired"
 })
 
