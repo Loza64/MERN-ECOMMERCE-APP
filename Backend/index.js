9933e00b0f63013e16f40ac508b275220731d6a2
Roberto Loza <76535183+Robert647@users.noreply.github.com>
2023-12-23 22:13:47 -0600

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,9 @@
 import debug from 'debug'
-import express from 'express'
 import { createServer } from 'http'
 import { PORT } from './src/Settings.js'
 import ServerApp from './src/Application.js'
 
-const message = debug('backend:[Server]')
-const ServerHttp = createServer(express().use(ServerApp))
+const Server = debug('backend:[Server]')
+const Http = createServer(ServerApp)
 
-ServerHttp.listen(PORT, () => { message(`is running on port: ${PORT}`) })
+Http.listen(PORT, () => { Server(`is running on port: ${PORT}`) })
