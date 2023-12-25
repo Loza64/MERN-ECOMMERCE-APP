514f49fa21f399ceff32026d13d9f67a30726992
Loza64 <robertoloxa88@gmail.com>
2023-12-24 20:21:04 -0600

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,6 @@ import Application from './src/Application.js'
 import { OptionsHttp, PORT } from './src/Settings.js'
 
 const Server = debug('backend:[Server]')
-const Http = createServer(OptionsHttp, Application, (req, res) => res.send(`Hello to server ecommerce`))
+const Http = createServer(OptionsHttp, Application)
 
 Http.listen(PORT, () => { Server(`is running on port: ${PORT}`) })
