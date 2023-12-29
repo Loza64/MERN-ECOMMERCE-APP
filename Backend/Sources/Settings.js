30b94415cbb49ceda04175f24e95b45fc132f234
Loza64 <robertoloxa88@gmail.com>
2023-12-29 17:49:08 -0600

    changes

--- Backend/Sources/Settings.js
+++ Backend/Sources/Settings.js
@@ -19,6 +19,7 @@ export const Origin = process.env.ORIGIN;
 export const TokenSecret = process.env.TOKEN;
 export const Session = process.env.SESSION;
 export const NodeEnv = process.env.NODE_ENV;
+export const Stripes = process.env.STRIPE;
 
 
 //Setting Application
