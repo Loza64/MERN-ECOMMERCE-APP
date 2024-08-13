290411ab029a45b191b88a0005c5c3ddb2b053fe
Roberto Loza <robertoloxa88@gmail.com>
2024-08-12 20:27:01 -0600

    fix error

--- Backend/Sources/Middlewares/CheckSession.js
+++ Backend/Sources/Middlewares/CheckSession.js
@@ -3,27 +3,27 @@ import { VerifyToken } from "../Libraries/jsonwebtoken.js"
 import { Users } from "../Models/Model.js"
 
 export const isAutenticate = async (req, res, next) => {
-    if ((req.session && req.session.token)) {
-        const token = await VerifyToken(req.session.token)
-        if (token) {
-            const profile = await Users.findById(token)
-            if (profile) {
-                next()
-                Session("authorized")
+    try {
+        if (req.session) {
+            const token = await VerifyToken(req.session.token)
+            if (token) {
+                const profile = await Users.findById(token)
+                if (profile) {
+                    Session("authorized")
+                } else {
+                    Session("has removed")
+                    req.session.destroy()
+                    return res.status(410).json({ state: false, message: "Your account has been deleted due to violations of our policies." }) 
+                }
             } else {
-                Session("has removed")
-                next('Session has removed')
-                req.session.destroy()
-                return res.status(410).json({ state: false, message: "Your account has been deleted due to violations of our policies." }) 
+                Session("not authorized or not exist")
+                return res.status(401).json({ state: false, message: "Your session has been finished." })
             }
         } else {
             Session("not authorized or not exist")
-            next('Session not authorized or not exist')
             return res.status(401).json({ state: false, message: "Your session has been finished." })
         }
-    } else {
-        Session("not authorized or not exist")
-        next('Session not authorized or not exist')
-        return res.status(401).json({ state: false, message: "Your session has been finished." })
+    } catch (error) {
+        return res.status(500).json({ state: false, message: error.message })
     }
 }
