fb5df5f96a8d63da224f11ac4078121f477bcfad
Roberto Loza <loza@ASUS-X515EA.ufostartdevelopment.com>
2024-09-10 10:32:19 -0600

    changes

--- Backend/Sources/Libraries/nodemailer.js
+++ Backend/Sources/Libraries/nodemailer.js
@@ -1,15 +1,18 @@
 import nodemailer from 'nodemailer'
+import smtp from 'nodemailer-smtp-transport'
 import { MAIL_BUSINESS, MAIL_PASS } from '../Config.js'
 
-const transport = nodemailer.createTransport({
-  service: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: MAIL_BUSINESS,
-    pass: MAIL_PASS
-  }
-})
+const transport = nodemailer.createTransport(
+  smtp({
+    service: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: MAIL_BUSINESS,
+      pass: MAIL_PASS
+    }
+  })
+)
 
 export default function SendEmail(mailmessage) {
   transport.sendMail(mailmessage, (error, info) => {
